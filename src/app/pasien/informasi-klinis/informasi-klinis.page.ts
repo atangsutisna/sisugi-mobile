0596c0f49b7f3f0720148af19fb4f8a1ff2c063f
Atang Sutisna <atang.sutisna.87@gmail.com>
2021-08-17 08:47:11 +0000

    get info pasien

--- src/app/pasien/informasi-klinis/informasi-klinis.page.ts
+++ src/app/pasien/informasi-klinis/informasi-klinis.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { PasienService } from 'src/app/pasien.service';
+import { Pasien } from 'src/app/pasien.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-informasi-klinis',
@@ -6,10 +9,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./informasi-klinis.page.scss'],
 })
 export class InformasiKlinisPage implements OnInit {
+  pasien: Pasien = {
+    id: null,
+    idKasus: null,
+    puskesmasId: null,
+    puskesmasNama: null,
+    nik: null,
+    nama: null,
+    vaksinasi: null,
+    tglDosis1: null,
+    tglDosis2: null,
+    tanggalLahir: null,
+    jenisKelamin: null,
+    kewarganegaraan: null,
+    pekerjaan: null,
+    ketPekerjaanLainnya: null,
+    nohp: null,
+    jalan: null,
+    rt: null,
+    rw: null,
+    kelurahanId: null,
+    kelurahanNama: null,
+    kecamatanId: null,
+    kecamatanNama: null,
+    kabkotaId: null,
+    kabkotaNama: null,
+    provinsiId: null,
+    provinsiNama: null,
+    latitude: null,
+    longitude: null,
+    statusEpidemiologi: null,
+    statusAkhir: null,
+    updatedAt: null,
+    createdAt: null,
+  };
+  constructor(
+    private pasienService: PasienService,
+    private activateRoute: ActivatedRoute
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    const id = this.activateRoute.snapshot.params.id;
+    this.pasienService.fetch(id).subscribe((pasien) => {
+      this.pasien = pasien;
+    });
   }
-
 }
