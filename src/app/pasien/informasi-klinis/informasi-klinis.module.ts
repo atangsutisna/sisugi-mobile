259a75c1b2ec9b5cde4ccca2c9e76f68d098d1e7
Atang Sutisna <atang.sutisna.87@gmail.com>
2021-08-17 09:35:58 +0000

    fix form informasi klinis

--- src/app/pasien/informasi-klinis/informasi-klinis.module.ts
+++ src/app/pasien/informasi-klinis/informasi-klinis.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -11,10 +11,10 @@ import { InformasiKlinisPage } from './informasi-klinis.page';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    InformasiKlinisPageRoutingModule
+    InformasiKlinisPageRoutingModule,
   ],
-  declarations: [InformasiKlinisPage]
+  declarations: [InformasiKlinisPage],
 })
 export class InformasiKlinisPageModule {}
--- src/app/pasien/informasi-klinis/informasi-klinis.page.ts
+++ src/app/pasien/informasi-klinis/informasi-klinis.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PasienService } from 'src/app/pasien.service';
 import { Pasien } from 'src/app/pasien.model';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-informasi-klinis',
@@ -43,17 +44,102 @@ export class InformasiKlinisPage implements OnInit {
     updatedAt: null,
     createdAt: null,
   };
+  form: FormGroup;
   constructor(
     private pasienService: PasienService,
     private activateRoute: ActivatedRoute
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.form = new FormGroup({
+      terdapatGejala: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      tanggalGejala: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      demam: new FormControl(null),
+      batuk: new FormControl(null),
+      pilek: new FormControl(null),
+      sakitTenggorokan: new FormControl(null),
+      sesakNapas: new FormControl(null),
+      sakitKepala: new FormControl(null),
+      lemas: new FormControl(null),
+      nyeriOtot: new FormControl(null),
+      mualMuntah: new FormControl(null),
+      nyeriAbdomen: new FormControl(null),
+      diare: new FormControl(null),
+      gangguanMenghidu: new FormControl(null),
+      gangguanMengecap: new FormControl(null),
+      gejalaLainnya: new FormControl(null),
+      ketGejalaLainnya: new FormControl(null),
+      hamil: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      diabetes: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      penyakitJantung: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      hipertensi: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      keganasan: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      gangguanImmunologi: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      gagalGinjal: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      gagalHati: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      ppok: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      komorbidLainnya: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      ketKomorbidLainnya: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      dirawatdirs: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      namars: new FormControl(null),
+      tanggalMasukRs: new FormControl(null),
+      icu: new FormControl(null),
+      inturbasi: new FormControl(null),
+      emco: new FormControl(null),
+      statusTerakhir: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      pneumonia: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      ards: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      lainnya: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      ketLainnya: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+    });
+  }
 
   ionViewWillEnter() {
     const id = this.activateRoute.snapshot.params.id;
     this.pasienService.fetch(id).subscribe((pasien) => {
       this.pasien = pasien;
     });
+  }
+
+  onSave() {
+    console.log('store on database');
   }
 }
