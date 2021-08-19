0bbdbd87eb2459e468d3969bf8e66e2f7dbcab2e
Atang Sutisna <atang.sutisna.87@gmail.com>
2021-08-19 08:04:29 +0700

    add pasien

--- src/app/pasien/informasi-pemeriksaan-penunjang/informasi-pemeriksaan-penunjang.page.ts
+++ src/app/pasien/informasi-pemeriksaan-penunjang/informasi-pemeriksaan-penunjang.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController, LoadingController } from '@ionic/angular';
+import { Pasien } from 'src/app/pasien.model';
+import { PasienService } from 'src/app/pasien.service';
+import { InformasiKlinisService } from '../informasi-klinis/informasi-klinis.service';
 
 @Component({
   selector: 'app-informasi-pemeriksaan-penunjang',
@@ -7,11 +12,58 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./informasi-pemeriksaan-penunjang.page.scss'],
 })
 export class InformasiPemeriksaanPenunjangPage implements OnInit {
+  pasien: Pasien = {
+    id: null,
+    idKasus: null,
+    puskesmasId: null,
+    puskesmasNama: null,
+    nik: null,
+    nama: null,
+    vaksinasi: null,
+    tglDosis1: null,
+    tglDosis2: null,
+    tanggalLahir: null,
+    jenisKelamin: null,
+    kewarganegaraan: null,
+    pekerjaan: null,
+    ketPekerjaanLainnya: null,
+    nohp: null,
+    jalan: null,
+    rt: null,
+    rw: null,
+    kelurahanId: null,
+    kelurahanNama: null,
+    kecamatanId: null,
+    kecamatanNama: null,
+    kabkotaId: null,
+    kabkotaNama: null,
+    provinsiId: null,
+    provinsiNama: null,
+    latitude: null,
+    longitude: null,
+    statusEpidemiologi: null,
+    statusAkhir: null,
+    updatedAt: null,
+    createdAt: null,
+  };
   form: FormGroup;
-  constructor() { }
+  constructor(
+    private pasienService: PasienService,
+    private activateRoute: ActivatedRoute,
+    private informasiKlinisService: InformasiKlinisService,
+    private alertCtrl: AlertController,
+    private loadinCtrl: LoadingController
+  ) { }
 
   ngOnInit() {
     this.form = new FormGroup({
+    });
+  }
+
+  ionViewWillEnter() {
+    const id = this.activateRoute.snapshot.params.id;
+    this.pasienService.fetch(id).subscribe((pasien) => {
+      this.pasien = pasien;
     });
   }
 
